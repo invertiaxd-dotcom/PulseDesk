0d3ce621f62ba2a91ecebc5eaf7f489099a4a8fd
Chaitanya <invertiaxd@gmail.com>
2026-01-11 20:16:34 +0530

    #tomworks!

---
+++ PulseDesk/src/java/heyServelet.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.RequestDispatcher;
+
+/**
+ *
+ * @author chait
+ */
+public class heyServelet extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            String msgs = "Hello";
+            request.setAttribute("msg", msgs);
+            RequestDispatcher rd = request.getRequestDispatcher("hello.jsp");
+            rd.forward(request, response);
+            out.println("we're up!");
+        }
+    }
+
+}
---
+++ PulseDesk/web/hello.jsp
@@ -0,0 +1,17 @@
+<%-- 
+    Document   : hello
+    Created on : 11 Jan 2026, 8:11:40 pm
+    Author     : chait
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+        <h1>heyoooooo</h1>
+    </body>
+</html>
